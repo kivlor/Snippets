6f46e178c69c16c78c8f7d2382a1f9c10988c376
Andrew Gallagher <admin@kivlor.com>
2012-06-09 16:03:36 +1000

    A bunch of updates...
    Added an initize method and set some default vars
    Added emailing of approve link for admin
    Added the approve functionality

--- app/app.rb
+++ app/app.rb
@@ -4,31 +4,34 @@ require File.dirname(__FILE__) + '/models'
 module Snippets
 	
 	class App < Sinatra::Base
+				
+		# init
+		def initialize
+			super()
+			@site_title = 'Snippets'
+			@site_url = 'localhost:9393'
+		end
 		
 		# list snippets
 		get '/' do
-			# find all the snippets!
 			@snippets = Snippet.all(:approved => true, :limit => 10, :order => [:created.desc])
 			
 			erb :list
 		end
 		
-		# get snippet
+		# view snippet
 		get '/snippet/:id' do
 		
-			# find snippet
 			@snippet = Snippet.get(params[:id])
 			
-			# found? yay!
 			if @snippet
-			   erb :snippet
-			  
-			# hmmm, error then...
+			 	erb :snippet
+			 	
 			else
-			   @title = 'Ooops'
-			   @message = 'Snippet not found'
-			   
-			   erb :error
+			 	@title = 'Ooops'
+			 	@message = 'Snippet not found'
+			 	
+			 	erb :error
 			end
 		end
 		
@@ -40,14 +43,21 @@ module Snippets
 		# submit snippet
 		post '/submit' do
 			
-			# try and create snippet
-			snippet = Snippet.create(:title => params[:title], :snippet => params[:snippet], :created => Time.now, :updated => Time.now)
+			@snippet = Snippet.create(:title => params[:title], :snippet => params[:snippet], :created => Time.now, :updated => Time.now)
 			
-			# save was successful?
-			if snippet.save
+			if @snippet.save
+			
+				# email admin approve link
+				Pony.mail(
+					:to => 'kivlor@gmail.com',
+					:from => 'kivlor@gmail.com',
+					:subject => 'Approve new Snippet',
+					:html_body => "<a href=\"http://#{@site_url}/approve/#{@snippet.id}/#{@snippet.adminhash}\">Approve #{@snippet.title}</a>",
+					:body => "Approve #{@snippet.title} - http://#{@site_url}/approve/#{@snippet.adminhash}"
+				)
+				
 				redirect '/'
 			
-			# doh!
 			else
 				@error = 'Unable to insert snippet, try again...'
 			end
@@ -55,15 +65,33 @@ module Snippets
 			erb :submit
 		end
 		
+		# approve snippet
+		get '/approve/:id/:adminhash' do
+			
+			@snippet = Snippet.get(params[:id])
+			
+			if @snippet and @snippet.adminhash == params[:adminhash]
+				
+				@snippet.approved = true
+				@snippet.save
+				
+				redirect '/'
+			else
+				@title = 'Ooops'
+				@message = 'Snippet not found'
+				
+				erb :error
+			end
+		end
+		
 		# 404
-		not_found do  
+		not_found do	
 			status 404
 			
 			@title = 404
 			@message = 'Page not found'
 			
 			erb :error
-		end 
+		end
 	end
-	
 end
