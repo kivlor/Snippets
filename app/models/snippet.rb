3e6868d23a52ef0b854071df7df66941f48205e6
Andrew Gallagher <admin@kivlor.com>
2012-06-09 16:00:36 +1000

    Added an admin hash and before save method to generate said hash

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -4,14 +4,21 @@ module Snippets
 	
 		include DataMapper::Resource
 		
+		before		:save, 		:createHash
+		
 		property	:id,		Serial
 		property	:title,		String,		:required => true
 		property	:snippet,	Text,		:required => true
 		
-		property	:approved,	Boolean,	:default => true
+		property	:adminhash,	String
+		property	:approved,	Boolean,	:default => false
 		
 		property	:created,	DateTime
 		property	:updated,	DateTime
+		
+		def createHash()
+			self.adminhash = Digest::MD5.hexdigest("#{Time.now.to_f}#{self.title}")
+		end
 		
 	end
 
